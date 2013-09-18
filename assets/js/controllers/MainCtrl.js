fec0023d59b045cd31643eaa15be09402019b4f5
George Wilson <george@g-wilson.co.uk>
2013-09-18 12:21:07 +0100

    result listing improvements

--- assets/js/controllers/MainCtrl.js
+++ assets/js/controllers/MainCtrl.js
@@ -6,14 +6,30 @@ autocar.controller('MainCtrl', function($scope, SearchIndex) {
 
   	$scope.results = data;
 
+    var pagesShown = 1;
+    var pageSize = 10;
+
+    $scope.itemsLimit = function() {
+        return pageSize * pagesShown;
+    };
+
+    $scope.showMore = function() {
+      pagesShown = pagesShown + 1;  
+    }
+
+    $scope.clearSearch = function() {
+      $scope.query = '';
+      pagesShown = 1;
+    }
+
+    $scope.expandListing = function(ev) {
+      angular.element(ev.srcElement).parent().parent().toggleClass('result-large');
+      var imgUrl = angular.element(ev.srcElement).parent().parent().find('img').attr('data-src');
+      angular.element(ev.srcElement).parent().parent().find('img').attr('src',imgUrl);
+    }
+
   }).error(function() {
     console.log('error getting car list');
   })
-
-  $scope.expandListing = function(ev) {
-    angular.element(ev.srcElement).parent().parent().toggleClass('result-large');
-    var imgUrl = angular.element(ev.srcElement).parent().parent().find('img').attr('data-src');
-    angular.element(ev.srcElement).parent().parent().find('img').attr('src',imgUrl);
-  }
 
 });
